230019feee5a20ea532572fd674eaaf5d3c98742
Derek <git@derekball.net>
2025-08-31 16:13:56 -0400

    Added ability for gemini to call get_files_info

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 from constants import *
+from functions.get_files_info import schema_get_files_info
 
 load_dotenv()
 api_key=os.environ.get("GEMINI_API_KEY")
@@ -26,13 +28,36 @@ def main():
     verbose = None
     if "--verbose" in sys.argv:
         verbose = True
+
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
+
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
         
-    content = client.models.generate_content(model=GEMINI_MODEL, contents=user_prompt)
-    print(content.text)
+    response = client.models.generate_content(
+        model=GEMINI_MODEL,
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt, tools=[available_functions]),
+    )
+    print(response.text)
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
     if verbose:
         print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {content.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {content.usage_metadata.candidates_token_count}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
